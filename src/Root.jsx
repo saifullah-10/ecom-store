70dc4e355cd5a615b2d5036ebf5524e277b2faef
saifullah-10 <saifullah.bus@gmail.com>
2024-08-14 21:47:00 +0600

    initial setup

---
+++ src/Root.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/home/Home";
+
+export default function Root() {
+  return (
+    <section>
+      <div>
+        <Home />
+      </div>
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import Root from "./Root.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
