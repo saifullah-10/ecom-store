9d9fda9659266e41cc199042176b37d102c01329
saifullah-10 <saifullah.bus@gmail.com>
2024-08-15 16:54:04 +0600

    create product Card

--- src/pages/products/components/ProductCard.jsx
+++ src/pages/products/components/ProductCard.jsx
@@ -1,30 +1,94 @@
 export default function ProductCard() {
   return (
     <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className=" p-4 border-2 cursor-pointer rounded-xl">
+        <div className=" mb-5">
+          <img
+            className=" w-full h-40 object-contain bg-cover"
+            src="https://www.startech.com.bd/image/cache/catalog/mouse/thunderobot/ml201/ml201-01%20(1)-228x228.webp"
+            alt=""
+          />
         </div>
-        <div>
+        <div className=" flex my-2 justify-between">
           <div>
             <h2>Product Name</h2>
           </div>
           <div>
-            <p>Price</p>
+            <p>39 $</p>
           </div>
         </div>
         <div>
-          <p>Category</p>
+          <p>Category: Electronics</p>
         </div>
-        <div>
-          <p>Description</p>
+        <div className=" my-2">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla</p>
         </div>
-        <div>
+        <div className=" flex justify-between">
           <div>
-            <p>ratings</p>
+            <ul className=" flex">
+              <li>
+                <svg
+                  className="w-4 h-4 text-yellow-300 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </li>
+              <li>
+                {" "}
+                <svg
+                  className="w-4 h-4 text-yellow-300 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </li>
+              <li>
+                {" "}
+                <svg
+                  className="w-4 h-4 text-yellow-300 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </li>
+              <li>
+                {" "}
+                <svg
+                  className="w-4 h-4 text-yellow-300 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </li>
+              <li>
+                {" "}
+                <svg
+                  className="w-4 h-4 text-gray-300 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </li>
+            </ul>
           </div>
           <div>
-            <p>Date</p>
+            <p>Date: 12/08/2024</p>
           </div>
         </div>
       </div>
